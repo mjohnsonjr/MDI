fb94048d199d0bdf76fc04501df62262be838a70
michael <michael@dogkat.com>
2014-08-24 22:03:03 -0500

    fixeddddd

--- src/sputnik/server/logic/PageTurner.java
+++ src/sputnik/server/logic/PageTurner.java
@@ -2,6 +2,7 @@ package sputnik.server.logic;
 
 import java.util.Vector;
 
+import sputnik.server.ConnectionManager;
 import sputnik.server.IOManager;
 import sputnik.server.util.Connection;
 import sputnik.server.util.Tick;
@@ -14,11 +15,13 @@ public class PageTurner implements Runnable {
 	private Tick tick;
 	private ServerLogic serverLogic;
 	private IOManager ioManager;
+	private ConnectionManager connectionManager;
 	
 	/* Constructors */
 	public PageTurner( ServerLogic serverLogic, double nsPerTick, Vector<Connection> connections, int port ) {
 		this.thread = new SThread(this);
 		this.ioManager = new IOManager( connections, port );
+		this.connectionManager = new ConnectionManager( connections, port );
 		this.serverLogic = serverLogic;
 		this.tick = new Tick( nsPerTick );
 	}
@@ -31,6 +34,11 @@ public class PageTurner implements Runnable {
 	
 	public void start(){
 		ThreadHandler.startThread( thread );
+	}
+	
+	public void startDefaults(){
+		this.ioManager.start();
+		this.connectionManager.start();
 	}
 	
 	public void stop() {
